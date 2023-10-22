60644f7947bbc54ee0729dbb898b210fbe20aa34
František Křížek <fkrizekk@gmail.com>
2023-10-22 04:37:27 +0200

    dasdgsr

--- Virmalous/Assets/Scripts/GameData.cs
+++ Virmalous/Assets/Scripts/GameData.cs
@@ -26,13 +26,13 @@ public class Assignment
 public class GameData
 {
 	//Controls
-	public float MouseSensitivity = 1f;
+	public float MouseSensitivity = 1.5f;
 
 	//Audio
-	public float MasterVol = 0.5f;
-	public float SfxVol = 0.5f;
-	public float MusicVol = 0.5f;
-	public float SpeechVol = 0.5f;
+	public float MasterVol = -30f;
+	public float SfxVol = -30f;
+	public float MusicVol = -30f;
+	public float SpeechVol = -30f;
 	
 	//Progress
 	
